fecd2d495c7404ff83116246cf36f1858aee2202
Nat34 <nathan.a.thomas@gmail.com>
2016-04-28 12:21:50 -0400

    init commit

---
+++ res-web/javascripts/studio-app.js
@@ -0,0 +1,8 @@
+var main = function () {
+	"use strict";
+
+	window.alert("Hello:  Please visit my Github repository project-permute for complete documentation on these digital works.")
+
+};
+
+$(document).ready(main);
